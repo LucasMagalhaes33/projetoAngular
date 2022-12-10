354c28178edbc360239fb02bbdbc66a45597c09c
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2022-12-10 12:09:13 -0300

    adicionando modulos de rotas para Home e Usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,27 @@
+import { HomeComponent } from './home/home.component';
 import { UsuarioComponent } from './usuario/usuario.component';
-import { HomeComponentComponent } from './home-component/home-component.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponentComponent},
-  {path: 'usuario', component: UsuarioComponent},
-  {path: '', pathMatch: 'full', redirectTo: '/home'}
+  {path: 'home',
+   loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {path: 'usuario',
+   loadChildren: () => import('./usuario/usuario.module').then(m => m.UsuarioModule)
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  }
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {useHash:true})
   ],
   exports: [
     RouterModule
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponentComponent } from './home-component/home-component.component';
-import { UsuarioComponent } from './usuario/usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     FooterComponent,
-    HomeComponentComponent,
-    UsuarioComponent
   ],
   imports: [
     BrowserModule,
